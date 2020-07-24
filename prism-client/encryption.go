6af0ec04f35a6e22d64cb703bab99e67a040c2f4
joshua-isak <isakj@bu.edu>
2020-07-23 20:30:51 -0400

    errors now print as messages

--- prism-client/encryption.go
+++ prism-client/encryption.go
@@ -39,26 +39,33 @@ func encrypt(data []byte, key []byte) []byte {
 
 // AES decrypt bytes of data with a 32 byte key
 func decrypt(cipherData []byte, key []byte) []byte {
+    var errMsg string
+
 	c, err := aes.NewCipher(key)
     if err != nil {
-        fmt.Println("aes.NewCipher failed:", err)
+        errMsg += "aes.NewCipher: " + err.Error()
     }
 
     gcm, err := cipher.NewGCM(c)
     if err != nil {
-        fmt.Println("cipher.NewGCM failed:", err)
+        errMsg += "cipher.NewGCM: " + err.Error()
     }
 
     nonceSize := gcm.NonceSize()
     if len(cipherData) < nonceSize {
-        fmt.Println("cipherData is less than nonceSize")
+        errMsg += "cipherData is less than nonceSize"
     }
 
     nonce, ciphertext := cipherData[:nonceSize], cipherData[nonceSize:]
     plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
     if err != nil {
-        fmt.Println(err)
-	}
+        errMsg += "gcm.Open: " + err.Error()
+    }
+
+    // If an error occured, return that error as a byte array instead of the decrypted message
+    if len(errMsg) > 0 {
+        return []byte(errMsg)
+    }
 
 	return []byte(plaintext)
 }
